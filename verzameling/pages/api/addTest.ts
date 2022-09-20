6afbacad3b37d84cf9de8369e4b3c1cb4397d888
Cefas88136 <95617801+Cefas88136@users.noreply.github.com>
2022-09-20 16:14:27 +0200

    get data

---
+++ verzameling/pages/api/addTest.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PrismaClient, test } from '@prisma/client'
+const prisma = new PrismaClient()
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    if ('POST' !== req.method) {
+        return res.status(405).json({message: "Method not allowed."})
+    }
+    const data = JSON.parse(req.body)
+}
