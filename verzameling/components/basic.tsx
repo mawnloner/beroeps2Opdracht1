fb5def6034cfe43dda86253746a6a23565c74b72
Cefas88136 <95617801+Cefas88136@users.noreply.github.com>
2022-09-27 11:45:02 +0200

    indeling

---
+++ verzameling/components/basic.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function Header() {
+    return (
+        <header>
+            header
+        </header>
+    )
+}
+
+export function Footer() {
+    return (
+        <footer>
+            footer
+        </footer>
+    )
+}
