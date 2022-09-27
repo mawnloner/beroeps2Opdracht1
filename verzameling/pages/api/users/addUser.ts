6025fbf9de38dc07f079d47fc1f23a3746e78c43
RemcoHottentot <87954@glr.nl>
2022-09-27 13:10:49 +0200

    pull

---
+++ verzameling/pages/api/users/addUser.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, gebruikers } from "@prisma/client";
+
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    
+//     res.json()
+// }
+
+// import { createAccount } from "login.tsx";
+
--- verzameling/pages/login.tsx
+++ verzameling/pages/login.tsx
@@ -1,9 +1,34 @@
 import type { NextPage } from 'next'
 
-const Home: NextPage = ({ }) => {
+declare var userName: string;
+declare var password: string;
+
+async function createAccount() {
+    userName: userName;
+    password: password;
+}
+
+export {createAccount}; {
+    userName: userName;
+    password: password;
+}
+
+const Login: NextPage = ({ }) => {
     return(
         <div>
-            
+            <fieldset>
+            <legend>Maak een account</legend>
+                <form onSubmit={createAccount()} id="createAccount">
+                    <label htmlFor="userName">Name:</label>
+                    <input required type="text" name="userName" id="userName"/>
+                    <label htmlFor="password">Password:</label>
+                    <input required type="password" name="password" id="password"/>
+
+                    <input type="submit" value="Account aanmaken" />
+                </form>
+            </fieldset>
         </div>
     )
 }
+
+export default Login
