eb1a101dbe5b76ab0c2ce57aa06d40b5b180e49e
Jobbe87245 <93382049+Jobbe87245@users.noreply.github.com>
2022-09-27 16:00:12 +0200

    einde beroeps
    iets

--- verzameling/components/basic.tsx
+++ verzameling/components/basic.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 export function Header() {
     return (
         <header>
-            header
+            <ul>
+                <li className="links"><a href="">Contact</a></li>
+                <li className="links"><a href="">Onze Collectie</a></li>
+                <li className="midden"><a href=""><img src="/media/test.jpg" alt="" /></a></li>
+                <li className="rechts"><a href="">Winkelmand</a></li>
+                <li className="rechts"><a href="">Inloggen</a></li>
+            </ul>
         </header>
     )
 }
