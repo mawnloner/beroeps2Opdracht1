64109a8ae57f9c75fd9586f2300236bf04828144
Cefas88136 <95617801+Cefas88136@users.noreply.github.com>
2022-11-16 22:25:42 +0100

    link ipv a

--- verzameling/pages/api/bestellingen/getAll.ts
+++ verzameling/pages/api/bestellingen/getAll.ts
@@ -8,7 +8,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             gebruikers: true,
             products: {
                 include: {
-                    crystal: true
+                    kristal: true
                 }
             }
         }
